20279d5cc24c06fb55d64256fbf26d53e9ea8eb2
Shep-li <li.hanz@northeastern.edu>
2021-10-29 02:52:07 +0800

    -

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // connection instance
 const connection = mysql.createPool({
     connectionLimit:100,
-    host:process.env.DB_HOST,
+    host:"database-1.cjdefajqur3p.us-east-2.rds.amazonaws.com",
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD || '',
     database:process.env.DB_NAME
